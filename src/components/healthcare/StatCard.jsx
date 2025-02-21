22c7678752b5c9e463a56829b0f3e09a34786719
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2025-02-22 00:05:07 +0530

    feat: add StatCard and InjuryHistory components to Healthcare page

--- src/components/healthcare/StatCard.jsx
+++ src/components/healthcare/StatCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-export default function DietCard({ value, icon, subText }) {
+export default function StatCard({ value, icon, subText, perDay = true }) {
   return (
     <div className="inline-flex items-center bg-white px-12 py-8 gap-8 rounded-2xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.04)] w-fit">
       <img src={icon} alt="img" className="w-24" />
       <div>
         <h1 className="text-primary text-4xl font-bold ">{value}</h1>
         <p className="my-1">{subText}</p>
-        <p className="text-sm text-ptext">Per Day</p>
+        {perDay && <p className="text-sm text-ptext">Per Day</p>}
       </div>
     </div>
   );
