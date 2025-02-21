68cb8d50bc4c2fd0075dc41e66683753103d165b
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2025-02-21 17:02:51 +0530

    feat: add Track component to Landing page

---
+++ src/components/landing/Track.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Track() {
+  return (
+    <div className="bg-[#F3F7F6] w-full flex justify-center">
+      <div className="bg-[#F3F7F6] flex gap-10 px-auto py-[5rem] max-w-7xl items-center ">
+        <h1 className="text-5xl font-semibold text-primary leading-normal">
+          Track, manage, and customize athlete growth with ease
+        </h1>
+        <p className="font-light">
+          Discover a smart solution that accelerates athlete management and
+          performance tracking.
+        </p>
+      </div>
+    </div>
+  );
+}
